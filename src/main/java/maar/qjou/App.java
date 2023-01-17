9fe2ef9e5289d7f7dedb205f50bd965be378c1c6
Quentin Jouannot <quentin.jouannot@gmail.com>
2023-01-17 09:05:02 +0100

    Initial commit

---
+++ src/main/java/maar/qjou/App.java
@@ -0,0 +1,13 @@
+package maar.qjou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
